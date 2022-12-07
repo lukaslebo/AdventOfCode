4faf041612605495fcc3c5f1f3804bbec771ae50
Lukas Lebovitz <lukas.lebo@hotmail.com>
2022-12-07 07:39:40 +0100

    day 07

--- src/year2022/day07/Day07.kt
+++ src/year2022/day07/Day07.kt
@@ -6,8 +6,8 @@ import readInput
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("2022", "Day07_test")
-    check(part1(testInput), 0)
-    check(part2(testInput), 0)
+    check(part1(testInput), 95437)
+    check(part2(testInput), 24933642)
 
     val input = readInput("2022", "Day07")
     println(part1(input))
@@ -15,9 +15,45 @@ fun main() {
 }
 
 private fun part1(input: List<String>): Int {
-    return input.size
+    return parseDirectoriesByPath(input).values.filter { it.size <= 100_000 }.sumOf { it.size }
 }
 
 private fun part2(input: List<String>): Int {
-    return input.size
+    val directoriesByPath = parseDirectoriesByPath(input)
+    val usedSpace = directoriesByPath[listOf("/")]!!.size
+    val minSizeToDelete = usedSpace - 40_000_000
+    val sortedBySize = directoriesByPath.values.sortedBy { it.size }
+    val dirToDelete = sortedBySize.first { it.size >= minSizeToDelete }
+    return dirToDelete.size
 }
+
+private fun parseDirectoriesByPath(input: List<String>): Map<List<String>, File> {
+    val path = mutableListOf<String>()
+    return buildMap {
+        for (line in input) {
+            if (line == "$ cd ..") path.removeLast()
+            else if (line.startsWith("$ cd")) path += line.split(' ').last()
+            else if (line == "$ ls") put(path.toList(), File(path.toList()))
+            else {
+                val parent = get(path)!!
+                val (info, name) = line.split(' ')
+                val file = File(path + name, isFile = info != "dir", size = info.toIntOrNull() ?: 0)
+                parent.children += file
+                if (file.isFile) {
+                    val pathCopy = path.toMutableList()
+                    while (pathCopy.isNotEmpty()) {
+                        get(pathCopy)!!.size += file.size
+                        pathCopy.removeLast()
+                    }
+                } else put(file.path, file)
+            }
+        }
+    }
+}
+
+private data class File(
+    val path: List<String>,
+    val children: MutableList<File> = arrayListOf(),
+    val isFile: Boolean = false,
+    var size: Int = 0
+)
