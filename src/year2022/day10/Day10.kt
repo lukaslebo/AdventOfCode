40a3ba286325b65858dad29ae4997c989dfbf7e0
Lukas Lebovitz <lukas.lebo@hotmail.com>
2022-12-10 11:07:02 +0100

    prepare all files for day 1 through day 25

---
+++ src/year2022/day10/Day10.kt
@@ -0,0 +1,23 @@
+package year2022.day10
+
+import check
+import readInput
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("2022", "Day10_test")
+    check(part1(testInput), 0)
+    check(part2(testInput), 0)
+
+    val input = readInput("2022", "Day10")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}
---
+++ src/year2022/day21/Day21.kt
@@ -0,0 +1,23 @@
+package year2022.day21
+
+import check
+import readInput
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("2022", "Day21_test")
+    check(part1(testInput), 0)
+    check(part2(testInput), 0)
+
+    val input = readInput("2022", "Day21")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}
