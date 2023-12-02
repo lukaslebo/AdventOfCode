fc7950f462b0fe00165670f15aa83460257ca12a
Lukas Lebovitz <lukas.lebo@hotmail.com>
2023-12-02 07:31:36 +0100

    parse leaderboards

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.serialization") version "1.9.21"
 }
 
 sourceSets {
     main {
         kotlin.srcDir("src")
     }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 }
 
 tasks {
