a6ad1317cec2cb9b3751485df5c06240686f19eb
Lukas Lebovitz <lukas.lebo@hotmail.com>
2023-12-24 15:44:09 +0100

    day 24

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ sourceSets {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
+    implementation("tools.aqua:z3-turnkey:4.12.2.1")
 }
 
 tasks {
