261e1d543833927473fc6ba348a53ce65d25c662
Lukas Lebovitz <lukas.lebo@hotmail.com>
2024-10-22 19:09:20 +0200

    upgrade gradle, kotlin and other libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.serialization") version "1.9.21"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 sourceSets {
@@ -10,13 +10,13 @@ sourceSets {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
-    implementation("tools.aqua:z3-turnkey:4.12.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("tools.aqua:z3-turnkey:4.13.0.1")
 }
 
 tasks {
     wrapper {
-        gradleVersion = "8.5"
+        gradleVersion = "8.10.2"
     }
 }
