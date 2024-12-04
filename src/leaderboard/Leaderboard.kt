2a3053e200d074914278a02b610b6ce7b86931e9
Lukas Lebovitz <lukas.lebovitz@zkb.ch>
2024-12-04 07:40:38 +0100

    improve leaderboard analytics

--- src/leaderboard/Leaderboard.kt
+++ src/leaderboard/Leaderboard.kt
@@ -1,5 +1,9 @@
 package leaderboard
 
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -11,13 +15,9 @@ import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 import readProgramParams
 import requestEndpoint
-import java.time.Duration
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
 
-private const val showDuration = true
+private const val printStarsByUser = true
+private const val printRanksPerStar = true
 
 /**
  * Run with args: --session=abc --leaderboardUrl=https://adventofcode.com/2023/leaderboard/private/view/123.json
@@ -30,21 +30,72 @@ fun main(args: Array<String>) {
 
     val leaderboardJson = requestEndpoint(leaderboardUrl, session)
     val leaderboard = Json.decodeFromString<Leaderboard>(leaderboardJson)
-    leaderboard.members.values.filter { it.localScore > 0 }.sortedByDescending { it.localScore }.forEach { member ->
-        val year = leaderboard.event.toInt()
-        print("\n${member.name ?: "Anonymous ${member.id}"}")
-        member.starsByDay.entries.sortedBy { it.key }.forEach { (day, stars) ->
-            val (star1, star2) = stars
-            fun Star.time() = if (showDuration) tms.duration(year, day) else tms.formatted()
-            print("\nDay $day: Star 1 ${star1.time()}")
-            if (star2 != null) {
-                print(" | Star 2 ${star2.time()}")
-            }
-        }
-    }
+    if (printStarsByUser) leaderboard.printStarDurationsPerMember()
+    if (printRanksPerStar) leaderboard.printRanksPerStar()
 
     val owner = leaderboard.members[leaderboard.ownerId]
     println("\n\nFor Leaderboard ${owner?.name} with ${leaderboard.members.size} members")
+}
+
+private fun Leaderboard.printStarDurationsPerMember() {
+    members.values.filter { it.localScore > 0 }.sortedByDescending { it.localScore }.forEach { member ->
+        val year = event.toInt()
+        print("\n${member.nameOrId}")
+        member.starsByDay.entries.sortedBy { it.key }.forEach { (day, stars) ->
+            val (star1, star2) = stars
+            print("\nDay $day: Star 1 ${star1.tms.duration(year, day)}")
+            if (star2 != null) {
+                print(" | Star 2 ${star2.tms.duration(year, day)}")
+            }
+        }
+    }
+}
+
+private fun Leaderboard.printRanksPerStar() {
+    val starDetailsList = members.values.flatMap { member ->
+        member.starsByDay.entries.flatMap { (day, star) ->
+            val start = LocalDateTime.of(event.toInt(), 12, day, 6, 0, 0)
+            listOfNotNull(
+                StarDetails(
+                    name = member.nameOrId,
+                    day = day,
+                    star = 1,
+                    duration = Duration.between(start, star.star1.tms),
+                ),
+                if (star.star2 != null) StarDetails(
+                    name = member.nameOrId,
+                    day = day,
+                    star = 2,
+                    duration = Duration.between(start, star.star2.tms),
+                ) else null,
+            )
+        }
+    }.sortedBy { it.duration }
+
+    println("\n\n")
+    val sortedGroups = starDetailsList.groupBy { it.day to it.star }.entries.sortedBy { it.key.first * 10 + it.key.second }
+    val maxNameLength = starDetailsList.maxOf { it.name.length }
+    for ((key, list) in sortedGroups) {
+        val (day, star) = key
+        println("-".repeat(50))
+        println("Day $day Star $star")
+        list.take(10).forEachIndexed { i, it ->
+            val rank = "${i + 1}".padEnd(list.size.toString().length, ' ')
+            val paddedName = it.name.padEnd(maxNameLength, ' ')
+            println("$rank $paddedName ${it.durationText}")
+        }
+    }
+    println("-".repeat(50))
+}
+
+private data class StarDetails(
+    val name: String,
+    val day: Int,
+    val star: Int,
+    val duration: Duration,
+) {
+    val durationText: String
+        get() = duration.format()
 }
 
 @Serializable
@@ -53,6 +104,9 @@ private data class Leaderboard(
     @SerialName("owner_id")
     val ownerId: Long,
     val members: Map<Long, Member>,
+    @SerialName("day1_ts")
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val day1Ts: LocalDateTime,
 )
 
 @Serializable
@@ -69,7 +123,10 @@ private data class Member(
     val lastStarTs: LocalDateTime,
     @SerialName("completion_day_level")
     val starsByDay: Map<Int, Stars>,
-)
+) {
+    val nameOrId: String
+        get() = name ?: "Anonymous $id"
+}
 
 @Serializable
 private data class Stars(
@@ -101,15 +158,14 @@ private object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
     }
 }
 
-private val format = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")
-private fun LocalDateTime.formatted() = format(format)
 private fun LocalDateTime.duration(year: Int, day: Int): String {
     val start = LocalDateTime.of(year, 12, day, 6, 0, 0)
-    val duration = Duration.between(start, this)
-    return buildString {
-        val hours = duration.toHours().toString().padStart(2, '0')
-        val minutes = duration.toMinutesPart().toString().padStart(2, '0')
-        val seconds = duration.toSecondsPart().toString().padStart(2, '0')
-        append("$hours:$minutes:$seconds")
-    }
+    return Duration.between(start, this).format()
+}
+
+private fun Duration.format() = buildString {
+    val hours = toHours().toString().padStart(2, '0')
+    val minutes = toMinutesPart().toString().padStart(2, '0')
+    val seconds = toSecondsPart().toString().padStart(2, '0')
+    append("$hours:$minutes:$seconds")
 }
