3001a0e69865843b073bec413c7360b02e47b061
Lukas Lebovitz <lukas.lebo@hotmail.com>
2025-11-08 10:47:31 +0100

    year 2017 day 21

--- common/src/Utils.kt
+++ common/src/Utils.kt
@@ -38,6 +38,13 @@ fun gcd(a: Long, b: Long): Long {
     return gcd(b, a % b)
 }
 
+fun <T> List<List<T>>.transpose() = this[0].indices.map { i -> indices.map { j -> this[j][i] } }
+fun <T> List<List<T>>.rotate90() = transpose().map { it.reversed() }
+fun <T> List<List<T>>.rotate270() = map { it.reversed() }.transpose()
+fun <T> List<List<T>>.rotate180() = map { it.reversed() }.reversed()
+fun <T> List<List<T>>.mirrorLeftRight() = map { it.reversed() }
+fun <T> List<List<T>>.mirrorTopDown() = reversed()
+
 fun List<String>.splitByEmptyLines(): List<List<String>> =
     joinToString("\n")
         .split("\n\n")
